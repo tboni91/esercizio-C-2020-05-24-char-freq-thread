e4c9b2354ea43d712c1c7669a70cd85926c569e1
tboni91 <57482424+tboni91@users.noreply.github.com>
2020-05-28 15:18:14 +0200

    Update esercizio-C-2020-05-24-char-freq-thread.c

--- src/esercizio-C-2020-05-24-char-freq-thread.c
+++ src/esercizio-C-2020-05-24-char-freq-thread.c
@@ -5,7 +5,8 @@
 
 char * content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis rhoncus urna neque viverra justo nec ultrices. Pretium quam vulputate dignissim suspendisse in est ante. Vitae congue mauris rhoncus aenean. Blandit cursus risus at ultrices mi. Ut lectus arcu bibendum at varius vel pharetra vel. Etiam non quam lacus suspendisse faucibus interdum posuere. Eget sit amet tellus cras adipiscing enim eu turpis egestas. Lectus magna fringilla urna porttitor rhoncus dolor purus non. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Nec tincidunt praesent semper feugiat nibh. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada.";
 
-il programma utilizza due thread aggiuntivi, il primo thread analizza i caratteri nelle posizioni "pari" della stringa (content[0], content[2], content[4]...)
+il programma utilizza due thread aggiuntivi, il primo thread analizza i caratteri nelle posizioni "pari" della stringa 
+(content[0], content[2], content[4]...)
 mentre il secondo considera i caratteri nelle posizioni "dispari" della stringa (content[1], content[3], content[5]...)
 
 definire un array condiviso:
